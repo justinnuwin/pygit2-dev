62b23d37e13fd8585aa307f81242e52dec50b52c
Justin Nguyen <justinnuwin@gmail.com>
2021-06-29 11:08:31 -0700

    Add install script for POSIX systems

---
+++ dep-repo/Linux_build.md
@@ -0,0 +1,8 @@
+# Pygit2 *Nix Build Instructions
+
+Sample script which builds libgit2 and pygit2 is provided in `scripts/`.
+
+## Requirements
+
+- CMake (v2.8+ <- libgit2)
+- gcc
---
+++ dep-repo/scripts/posix_build_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Ensure that CWD is scripts/
+# If pygit2 is to be installed in a virtual environment, make sure the venv is activated before running
+pushd ..
+LIBGIT2=$(pwd)/build
+mkdir -p $LIBGIT2
+
+git submodule update --init
+
+mkdir -p libgit2/build
+pushd libgit2/build
+cmake .. -DCMAKE_INSTALL_PREFIX=$LIBGIT2 -DUSE_SSH=ON -DBUILD_CLAR=OFF
+cmake --build . --target install
+popd
+
+pushd pygit2
+CPATH=$LIBGIT2/include LIBRARY_PATH=$LIBGIT2/lib* CFLAGS="-O0 -g -std-c11" python3 build_ext --inplace
+python3 install
+popd
+
+popd
